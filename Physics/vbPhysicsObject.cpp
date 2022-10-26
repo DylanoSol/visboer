c80f60b6ac6daafe3247726a6c487583aca5b832
DylanoSol <dylanosolter@hotmail.com>
2022-10-26 15:18:14 +0200

    [CHANGED] Set up object types based on mass

--- Physics/vbPhysicsObject.cpp
+++ Physics/vbPhysicsObject.cpp
@@ -1,6 +1,27 @@
 #include "vbPhysicsObject.h"
 
-vbPhysicsObject::vbPhysicsObject()
+vbPhysicsObject::vbPhysicsObject(vbCollisionShape* shape, float mass)
 {
+	m_shape = shape; 
+	m_mass = mass; 
 
+	if (m_mass > 0 + 0.000000001f)
+	{
+		m_objectType = vbObjectType::DYNAMIC_OBJECT; 
+	}
+	else if (m_mass < 0 - 0.000000001f)
+	{
+		m_objectType = vbObjectType::TRIGGER_VOLUME; 
+	} 
+	else
+	{
+		m_objectType = vbObjectType::STATIC_OBJECT; 
+	}
+
+}
+
+
+void vbPhysicsObject::SetObjectType(vbObjectType type)
+{
+	m_objectType = type; 
 }
--- Physics/vbPhysicsObject.h
+++ Physics/vbPhysicsObject.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../Primitives/vbCollisionShape.h"
+
 enum class vbObjectType
 {
 	UNDEFINED = 1 << 0,
@@ -11,7 +13,14 @@ enum class vbObjectType
 class vbPhysicsObject
 {
 public: 
-	vbPhysicsObject(); 
+	//Make sure to pass a shape and a mass. Read the documentation to see what certain masses do. 
+	vbPhysicsObject(vbCollisionShape* shape, float mass); 
+
+	vbCollisionShape* m_shape = nullptr;
+	float m_mass = 0.f; 
+
+	//This function solely exists as a safeguard. Do not use this to create dynamic objects. 
+	void SetObjectType(vbObjectType type); 
 
 private: 
 	vbObjectType m_objectType = vbObjectType::UNDEFINED; 
